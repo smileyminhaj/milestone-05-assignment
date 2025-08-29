2bfe783c08be24ba8591e0795e48723e37e4d4b0
smileyminhaj <smileyminhaj@gmail.com>
2025-08-29 23:33:23 +0600

    Added dom and sidebar

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const popupMenu = document.getElementById('popupMenu');
 menuToggle.addEventListener('click', () => {
   popupMenu.classList.toggle('hidden');
 });
+
+//!===========Credit Logic
+let tokens = 100;
+const tokenSpan = document.querySelector(".creditcount");
+const callButtons = document.querySelectorAll(".call-btn");
+
+callButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (tokens >= 20) {
+      tokens -= 20;
+      tokenSpan.textContent = tokens;
+    } else {
+      alert("❌ পর্যাপ্ত ব্যালেন্স নেই!");
+    }
+  });
+});
