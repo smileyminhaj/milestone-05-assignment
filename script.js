3e519539d85ad3123f6713b22068bcf6b393bbe5
smileyminhaj <smileyminhaj@gmail.com>
2025-08-29 15:54:44 +0600

    Made the navbar with toggle button

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menuToggle');
+const popupMenu = document.getElementById('popupMenu');
+
+menuToggle.addEventListener('click', () => {
+  popupMenu.classList.toggle('hidden');
+});
